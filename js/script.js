052eedf75141592ab021164f332f1972be78f7a2
masudrana2803 <masudrana2803@gmail.com>
2025-07-31 18:49:22 +0600

    API

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 fetch('https://dummyjson.com/products')
 .then(alu => alu.json())
 .then((data)=>{
-    data.map((item)=>{
+    data.products.map((item)=>{
         console.log(item)
+
+        let maindiv = document.createElement('div')
+        maindiv.className = 'maindiv'
+        createElement('h2', maindiv, item.title)
+        createElement('h3', maindiv, item.description)
+        createElement('h4', maindiv, `Price: $${item.price}`)
+        createElement('h5', maindiv, '', item.stock)
+
+
+        maindiv.appendChild
+
+
     })
 })
