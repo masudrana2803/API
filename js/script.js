5cd9834c568d5ac0e646e038f0e3d23fa86f3a9d
masudrana2803 <masudrana2803@gmail.com>
2025-07-31 19:44:25 +0600

    API

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
-fetch('https://dummyjson.com/products')
-.then(alu => alu.json())
-.then((data)=>{
-    data.products.map((item)=>{
-        console.log(item)
+let results = document.querySelector('.results');
+let allUsers = []; // Store fetched users
 
-        let maindiv = document.createElement('div')
-        maindiv.className = 'maindiv'
-        createElement('h2', maindiv, item.title)
-        createElement('h3', maindiv, item.description)
-        createElement('h4', maindiv, `Price: $${item.price}`)
-        createElement('h5', maindiv, '', item.stock)
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((alu) => alu.json())
+.then((data) => {
+    allUsers = data; // Save users for searching
+    displayResults(allUsers); // Show all users initially
+});
 
+function displayResults(users) {
+    results.innerHTML = ''; // Clear previous results
+    users.forEach((item) => {
+        let div = document.createElement('div');
+        let h2 = document.createElement('h2');
+        let h3 = document.createElement('h3');
+        let p = document.createElement('p');
 
-        maindiv.appendChild
+        div.classList.add('result1');
+        h2.innerHTML = 'User name: ' + item.name;
+        h3.innerHTML = 'User email: ' + item.email;
+        p.innerHTML = 'website: ' + item.website;
 
+        div.appendChild(h2);
+        div.appendChild(h3);
+        div.appendChild(p);
+        results.appendChild(div);
+    });
+}
 
-    })
-})
+function searchInfo() {
+    
+    const query = document.getElementById('searchInput').value.toLowerCase();
+    const filtered = allUsers.filter(user =>
+        user.name.toLowerCase().includes(query) ||
+        user.email.toLowerCase().includes(query) ||
+        user.website.toLowerCase().includes(query)
+    );
+    displayResults(filtered);
+
+}
